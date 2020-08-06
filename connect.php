445ae5bb0eba3e7b38950f02ed31783a41c12736
Toskgreg <toskingregory@icloud.com>
2020-08-06 11:09:46 +0300

    dds

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "", "ultimategym");
-    
-    if(!$conn){
-        echo "Error connecting to the database";
-        exit();
-    }
-return $conn;
